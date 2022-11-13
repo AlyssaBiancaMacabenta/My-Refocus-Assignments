f1aa879e3e31778dcf56db551675041429dfc27e
Alyssa Bianca Macabenta <alysizzle1@gmail.com>
2022-11-13 16:26:15 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,6 +1,18 @@
 function getBMI(weight, height) {
     let bmi = 703 * (weight / (height * height));
-    return bmi;
+    return Math.round(bmi * 100) / 100;
 }
 
-console.log(getBMI(154.32, 64.173))
+console.log(getBMI(154, 64))
+
+function testBMI() {
+    const result = getBMI(154, 64);
+    const expected = 26.43;
+
+console.assert(
+    result === expected, 
+    `The result ${result} does not match the expected value ${expected}`
+    )
+}
+
+testBMI();
